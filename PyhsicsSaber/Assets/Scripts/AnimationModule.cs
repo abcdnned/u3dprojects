5760019de03838e0ed09868e45e7708b36591c1d
tom <tom.yang01@sap.com>
2022-12-08 14:13:44 +0800

    update

--- PyhsicsSaber/Assets/Scripts/AnimationModule.cs
+++ PyhsicsSaber/Assets/Scripts/AnimationModule.cs
@@ -18,16 +18,17 @@ namespace TY {
             _initialJointsRotation = new Quaternion[_joints.Length];
             for (int i = 0; i < _joints.Length; i++) {
                 _initialJointsRotation[i] = _joints[i].transform.localRotation;
-                Debug.Log(this.GetType().Name + " _joints[i] " + i + " " + _joints[i]);
+                // Debug.Log(this.GetType().Name + " _joints[i] " + i + " " + _joints[i]);
             }
             for (int i = 0; i < _animatedBones.Length; ++i) {
-                Debug.Log(this.GetType().Name + " bone " + i + " " + _animatedBones[i]);
+                // Debug.Log(this.GetType().Name + " bone " + i + " " + _animatedBones[i]);
             }
         }
 
         void FixedUpdate() {
             UpdateJointTargets();
         }
+
 
         /// <summary> Makes the physical bones match the rotation of the animated ones </summary>
         private void UpdateJointTargets() {

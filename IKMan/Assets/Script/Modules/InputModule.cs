1d8cadd5b79f1a4dfe050c732e8423c2a9ea3f5a
tom <tom.yang01@sap.com>
2023-01-26 21:58:02 +0800

    update

---
+++ IKMan/Assets/Script/Modules/InputModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// Author: Sergio Abreu García | https://sergioabreu.me
+
+/// <summary> Tells the ActiveRagdoll what it should do. Input can be external (like the
+/// one from the player or from another script) and internal (kind of like sensors, such as
+/// detecting if it's on floor). </summary>
+public class InputModule : MonoBehaviour {
+    // ---------- EXTERNAL INPUT ----------
+
+    public delegate void onMoveDelegate(Vector2 movement);
+    public onMoveDelegate OnMoveDelegates { get; set; }
+    public void OnMove(InputValue value) {
+        OnMoveDelegates?.Invoke(value.Get<Vector2>());
+    }
+
+    void Start() {
+    }
+
+    void Update() {
+    }
+}
